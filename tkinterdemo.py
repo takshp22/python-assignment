85295b87c91b42a835a3a0a2b132a1d8420808eb
taksh <takshp2322@gmail.com>
2024-03-19 16:00:24 +0530

    19-3-2024

--- tkinterdemo.py
+++ tkinterdemo.py
@@ -1,6 +1,91 @@
 from tkinter import *
+import mysql.connector
+import tkinter.messagebox as msg
+
+def create_conn():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="python_22"
+        )
+def insert_data():
+    if e_fname.get()=="" or e_lname.get()=="" or e_email.get()=="" or e_mobile.get()=="":
+        msg.showinfo("insert satus"," All fields are mandatory")
+    else:
+        conn=create_conn()
+        cursor=conn.cursor()
+        query="insert into user (fname,lname,email,mobile)   values (%s,%s,%s,%s)"
+        args=(e_fname.get(),e_lname.get(),e_email.get(),e_mobile.get())
+        cursor.execute(query,args)
+        conn.commit()
+        conn.close()
+        e_fname.delete(0,'end')
+        e_lname.delete(0,'end')
+        e_email.delete(0,'end')
+        e_mobile.delete(0,'end')
+        msg.showinfo("insert status", "data inserted successfully")
+
+def search_data():
+    e_fname.delete(0,'end')
+    e_lname.delete(0,'end')
+    e_email.delete(0,'end')
+    e_mobile.delete(0,'end')
+    if e_id.get()=="":
+        msg.showinfo("search status","is is mandatory")
+    else:
+        conn=create_conn()
+        cursor=conn.cursor()
+        query="select * from user where id=%s"
+        args=(e_id.get(),)
+        cursor.execute(query,args)
+        row=cursor.fetchall()
+        if row:
+            e_fname.insert(0,row[0][1])
+            e_lname.insert(0,row[0][2])
+            e_email.insert(0,row[0][3])
+            e_mobile.insert(0,row[0][4])
+            
+        else:
+            msg.showinfo("search status","ID Not Found")
+        conn.close()
+        
+def update_data():
+    if e_fname.get()=="" or e_lname.get()=="" or e_email.get()=="" or e_mobile.get()=="" or e_id.get()=="":
+        msg.showinfo("update status","All feild are mandatory")
+    else:
+        conn=create_conn()
+        cursor=conn.cursor()
+        query="update user set fname=%s,lname=%s,email=%s,mobile=%s where id=%s"
+        args=(e_fname.get(),e_lname.get(),e_email.get(),e_mobile.get(),e_id.get())
+        cursor.execute(query,args)
+        conn.commit()
+        conn.close()
+        e_fname.delete(0,'end')
+        e_lname.delete(0,'end')
+        e_email.delete(0,'end')
+        e_mobile.delete(0,'end')
+        e_id.delete(0,'end')
+        msg.showinfo("update status","data updated successfully")
 
 
+def delete_data():
+    if e_id.get()=="":
+        msg.showinfo("delete status","id is mandatory")
+    else:
+        conn=create_conn()
+        cursor=conn.cursor()
+        query="delete from user where id=%s"
+        args=(e_id.get(),)
+        cursor.execute(query,args)
+        conn.commit()
+        conn.close()
+        e_fname.delete(0,'end')
+        e_lname.delete(0,'end')
+        e_email.delete(0,'end')
+        e_mobile.delete(0,'end')
+        e_id.delete(0,'end')
+        msg.showinfo("delete status","data deleted successfully")
 
 root=Tk()
 root.geometry("350x370")
@@ -38,14 +123,16 @@ e_email.place(x=150,y=200)
 e_mobile=Entry(root)
 e_mobile.place(x=150,y=250)
 
-insert=Button(root,text="INSERT",bg="black",fg="white",font=("forte",12))
+insert=Button(root,text="INSERT",bg="black",fg="white",font=("forte",12),command=insert_data)
 insert.place(x=20,y=300)
 
-search=Button(root,text="SEARCH",bg="black",fg="white",font=("forte",12))
+search=Button(root,text="SEARCH",bg="black",fg="white",font=("forte",12),command=search_data)
 search.place(x=95,y=300)
 
-update=Button(root,text="UPDATE",bg="black",fg="white",font=("forte",12))
+update=Button(root,text="UPDATE",bg="black",fg="white",font=("forte",12),command=update_data)
 update.place(x=174,y=300)
 
-delete=Button(root,text="DELETE",bg="black",fg="white",font=("forte",12))
+delete=Button(root,text="DELETE",bg="black",fg="white",font=("forte",12),command=delete_data)
 delete.place(x=252,y=300)
+
+root.mainloop()
